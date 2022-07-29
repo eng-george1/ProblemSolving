ee50683c9c6c09b80dc8a3cd73c40096d2e2142c
eng-george1 <eng.george1@gmail.com>
2022-07-28 19:04:45 -0500

    Sort, Bubble, Selection, Insertion

---
+++ Sorting/Main.java
@@ -0,0 +1,83 @@
+package Sorting;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.lang.model.type.ArrayType;
+
+public class Main {
+    public static void main(String[] args) {
+        // List<Integer> array=List.of(2,4,1,2,20,24,19,9,12,7);
+        // array.sort(null);
+        // System.out.println(array);
+        System.out.println("Start");
+        int[] array = { 3, 4, 1, 8, 5, 7, 2, 6, 0, -1, 100, 2, 5, 8, 9, 00, 22, 45 };
+        selectionSort(array);
+        System.out.println("Selection: " + Arrays.toString(array));
+        int[] array1 = { -2, 3, 4, 1, 8, 5, 7, 2, 6, 0, -1, 100, 2, 5, 8, 9, 00, 22, 45 };
+        bubbleSort(array1);
+        System.out.println("Bubble: " + Arrays.toString(array1));
+
+        int[] array3 = { -2, 3, 4, 1, 8, 5, 7, 2, 6, 0, -1, 100, 2, 5, 8, 9, 00, 22, 45 };
+        insertionSort(array3);
+        System.out.println("Insertion: " + Arrays.toString(array3));
+        selectionSort(new int[] { 1 });
+        selectionSort(new int[] {});
+        // System.out.println(Arrays.toString( ));
+    }
+
+    public static void bubbleSort(int[] array) {
+
+        for (int i = 0; i < array.length - 1; i++) {
+            boolean isSwapped = false;
+            for (int ii = 0; ii < array.length - 1 - i; ii++) {
+                if (array[ii] > array[ii + 1]) {
+                    // swap
+                    int tempForSwap = array[ii];
+                    array[ii] = array[ii + 1];
+                    array[ii + 1] = tempForSwap;
+                    isSwapped = true;
+                }
+            }
+            // if all items passed without any swap so its already sorted
+            // best case O(n)
+            if (!isSwapped)
+                break;
+        }
+
+    }
+
+    public static void selectionSort(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            int smalest = array[i];
+            int smalestIndex = i;
+            for (int ii = i + 1; ii < array.length; ii++) {
+
+                if (smalest > array[ii]) {
+                    // swap
+                    smalest = array[ii];
+                    smalestIndex = ii;
+                }
+            }
+            array[smalestIndex] = array[i];
+            array[i] = smalest;
+        }
+
+    }
+
+    public static void insertionSort(int[] array) {
+
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (array[j - 1] > array[j]) {
+                    int tepSwap = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = tepSwap;
+                } else
+                    break;
+            }
+        }
+
+    }
+}
