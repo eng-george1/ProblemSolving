8331cbd9ee48115edbb63034cdc5b63b19138bf4
eng-george1 <eng.george1@gmail.com>
2022-09-01 21:29:09 -0500

    Leet code Pinterest Preparation

--- LeetCode/Pinterest/Main.java
+++ LeetCode/Pinterest/Main.java
@@ -1,8 +1,14 @@
 package LeetCode.Pinterest;
 
+import java.lang.Thread.State;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.Stack;
 
 import javax.print.attribute.standard.RequestingUserName;
 
@@ -10,10 +16,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(reverse(1236676));
         System.out.println(isPalindrome2(121));
-        char[] s=new char[]{'a','b','c'};
+        char[] s = new char[] { 'a', 'b', 'c' };
         reverseString(s);
         System.out.println(Arrays.toString(s));
-        System.out.println(longestCommonPrefix(new String[]{"car","ca","carr"}));
+        System.out.println(longestCommonPrefix(new String[] { "car", "ca", "carr" }));
+        System.out.println(decodeString("abc3[cd]xyz"));
+        System.out.println(decodeString("3[a2[c]]"));
+        System.out.println(decodeString("3[a]2[bc]"));
+        System.out.println(groupAnagrams(new String[] { "eat", "tea", "tan", "ate", "nat", "bat" }));
+        System.out.println(groupAnagrams(new String[] { "a" }));
+
     }
 
     public static int reverse(int x) {
@@ -80,29 +92,162 @@ public class Main {
             return 1 / fast(x, n);
         return fast(x, n);
     }
+
     public static void reverseString(char[] s) {
-        int index=0;
-        while(index<=s.length/2){
-            char c= s[index];
-            s[index]=s[s.length-1-index];
-            s[s.length-1-index]=c;
+        int index = 0;
+        while (index <= s.length / 2) {
+            char c = s[index];
+            s[index] = s[s.length - 1 - index];
+            s[s.length - 1 - index] = c;
             index++;
         }
     }
 
     public static String longestCommonPrefix(String[] strs) {
-        String prefex=strs[0];
-        for(int i=1;i<strs.length;i++){            
-            while(prefex.length()>0){
-                if(strs[i].indexOf(prefex)==0)
-                break;
-                prefex=prefex.substring(0, prefex.length()-1);
-                if(prefex.isEmpty())
-                return "";
+        String prefex = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (prefex.length() > 0) {
+                if (strs[i].indexOf(prefex) == 0)
+                    break;
+                prefex = prefex.substring(0, prefex.length() - 1);
+                if (prefex.isEmpty())
+                    return "";
             }
         }
         return prefex;
     }
-    
 
+    public String longestPalindrome(String s) {
+        String maxStr = "";
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j < s.length(); j++) {
+                StringBuilder sb = new StringBuilder(s.substring(i, j + 1));
+                if (s.substring(i, j + 1).equals(sb.reverse())) {
+                    if (s.substring(i, j + 1).length() > maxStr.length())
+                        maxStr = s.substring(i, j + 1);
+                }
+            }
+        }
+        return maxStr;
+    }
+
+    static int startI = 0;
+    static int endI = 0;
+
+    public String longestPalindrome2(String s) {
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < s.length(); i++) {
+            expandarroundCenter(s, i, i);
+            expandarroundCenter(s, i, i + 1);
+
+        }
+        return s.substring(startI, endI + 1);
+    }
+
+    private static void expandarroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        if (endI - startI + 1 < right - left - 1) {
+            startI = left + 1;
+            endI = right - 1;
+        }
+    }
+
+    public static String decodeString(String s) {
+        Stack<Character> stack = new Stack<>();
+        StringBuilder result = new StringBuilder();
+        StringBuilder tempR = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ']') {
+                StringBuilder sb = new StringBuilder();
+                sb.insert(0, tempR.toString());
+                tempR = new StringBuilder();
+                while (stack.peek() != '[') {
+                    sb.insert(0, stack.pop());
+                }
+                stack.pop();
+                StringBuilder sbint = new StringBuilder();
+                char c = stack.peek();
+                while (!stack.isEmpty() && Character.isDigit(stack.peek())) {
+                    sbint.insert(0, stack.pop());
+                }
+                String temp = sb.toString();
+                if (sbint.isEmpty())
+                    sbint.append("0");
+                for (int j = 0; j < Integer.parseInt(sbint.toString()) - 1; j++) {
+                    sb.insert(0, temp);
+                }
+                if (stack.isEmpty()) {
+                    result.append(sb.toString());
+                } else
+                    tempR.insert(0, sb.toString());
+
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+        tempR = new StringBuilder();
+        while (!stack.isEmpty()) {
+            tempR.insert(0, stack.pop());
+        }
+        result.append(tempR.toString());
+        return result.toString();
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {      
+        Map<String, List<String>> map = new HashMap();
+        int[] count = new int[26];
+        Arrays.fill(count, 0);     
+        for (int i = 0; i < strs.length; i++) {
+            String l=convertStrtoKey(strs[i],count);
+            if(map.containsKey(l)){
+                map.get(l).add(strs[i]);
+            }
+            else{
+                List<String> list=new ArrayList<>();
+                list.add(strs[i]);
+                map.put(l,list);      
+            }
+        }       
+        return new ArrayList<>(map.values());
+    }
+
+    private static String convertStrtoKey(String s) {
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+    private static String convertStrtoKey(String s,int[] count) {
+        Arrays.fill(count, 0);     
+        for (char c : s.toCharArray()) count[c - 'a']++;
+        StringBuilder sb = new StringBuilder("");
+        for (int i = 0; i < 26; i++) {
+            sb.append('#');
+            sb.append(count[i]);
+        }
+        return sb.toString();
+    }
+
+    public static List<List<String>> groupAnagrams2(String[] strs) {
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        int[] count = new int[26];
+        for (String s : strs) {
+            Arrays.fill(count, 0);
+            for (char c : s.toCharArray()) count[c - 'a']++;
+
+            StringBuilder sb = new StringBuilder("");
+            for (int i = 0; i < 26; i++) {
+                sb.append('#');
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(s);
+        }
+        return new ArrayList(ans.values());
+    }
 }
---
+++ LeetCode/Pinterest/Main2.java
@@ -0,0 +1,223 @@
+package LeetCode.Pinterest;
+
+import java.lang.Thread.State;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.Stack;
+
+import javax.lang.model.util.ElementScanner14;
+
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println(longestValidParentheses2(")()())"));
+        System.out.println(reverseWords("  the   sky is   blue   "));
+        System.out.println(isAnagram("anagram", "nagaram"));
+        System.out.println(isAnagram("a", "ab"));
+        System.out.println(search(new int[] { 0, 2, 3, 4, 5, 6, 7, 8, 9, 9 }, 6));
+        System.out.println(findMin(new int[] { 4, 5, 6, 7, -1, 0, 1, 2 }));
+        System.out.println(findMin(new int[] { 11, 13, 15, 17 }));
+        System.out.println(findMin(new int[] { 5, 1, 2, 3, 4 }));
+        System.out.println(maxProfit(new int[] { 7, 1, 5, 3, 6, 4 })); 
+         System.out.println(maxProfit3(new int[] {7,1,5,3,6,4 }));  
+             System.out.println(maxProfit3(new int[] {1,2,3,4,5 }));
+             System.out.println(maxProfit3(new int[] {1,2,3,4,5 }));
+
+    }
+
+    public static int longestValidParentheses(String s) {
+        int maxL = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.length() - 1 - i < maxL)
+                return maxL;
+            for (int j = s.length() - 1; j > i; j--) {
+                if (isValid(s, i, j))
+                    maxL = Math.max(maxL, j - i + 1);
+            }
+        }
+        return maxL;
+    }
+
+    private static boolean isValid(String s, int start, int end) {
+        if ((end - start + 1) % 2 != 0)
+            return false;
+        Stack<Character> charStack = new Stack<>();
+        for (int i = start; i <= end; i++) {
+            if (s.charAt(i) == ')') {
+                if (charStack.isEmpty()) {
+                    return false;
+                }
+                charStack.pop();
+            } else {
+                charStack.push('(');
+            }
+        }
+        if (charStack.isEmpty())
+            return true;
+        return false;
+    }
+
+    public static int longestValidParentheses2(String s) {
+        int maxans = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.empty()) {
+                    stack.push(i);
+                } else {
+                    maxans = Math.max(maxans, i - stack.peek());
+                }
+            }
+        }
+        return maxans;
+    }
+
+    public static String reverseWords(String s) {
+        String[] words = s.split(" ");
+        StringBuilder result = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            if (!words[i].isEmpty())
+                result.append(words[i] + " ");
+        }
+        return result.toString().trim();
+    }
+
+    public static boolean isAnagram2(String s, String t) {
+        char[] charS = s.toCharArray();
+        Arrays.sort(charS);
+        char[] charT = t.toCharArray();
+        Arrays.sort(charT);
+        return Arrays.equals(charS, charT);
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+        char[] charS = new char[26];
+        char[] charT = new char[26];
+        for (int i = 0; i < s.length(); i++) {
+            charS[s.charAt(i) - 'a']++;
+            charT[t.charAt(i) - 'a']++;
+        }
+        return Arrays.equals(charS, charT);
+    }
+
+    public static int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            if (nums[(left + right) / 2] < target) {
+                left = (left + right) / 2 + 1;
+            } else if (nums[(right + left) / 2] > target) {
+                right = (right + left) / 2 - 1;
+            } else {
+                return (right + left) / 2;
+            }
+        }
+        return -1;
+    }
+
+    // rotated
+    public static int search2(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] >= nums[start]) {
+                if (target >= nums[start] && target < nums[mid])
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            } else {
+                if (target <= nums[end] && target > nums[mid])
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    // 4,5,6,7,0,1,2
+    public static int findMin(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int left = 0, right = nums.length - 1;
+        if (nums[right] > nums[0]) {
+            return nums[0];
+        }
+
+        while (right >= left) {
+            int mid = left + (right - left) / 2;
+
+            // if the mid element is greater than its next element then mid+1 element is the
+            // smallest
+            // This point would be the point of change. From higher to lower value.
+            if (nums[mid] > nums[mid + 1]) {
+                return nums[mid + 1];
+            }
+
+            // if the mid element is lesser than its previous element then mid element is
+            // the smallest
+            if (nums[mid - 1] > nums[mid]) {
+                return nums[mid];
+            }
+
+            // if the mid elements value is greater than the 0th element this means
+            // the least value is still somewhere to the right as we are still dealing with
+            // elements
+            // greater than nums[0]
+            if (nums[mid] > nums[0]) {
+                left = mid + 1;
+            } else {
+                // if nums[0] is greater than the mid value then this means the smallest value
+                // is somewhere to
+                // the left
+                right = mid - 1;
+            }
+        }
+        return Integer.MAX_VALUE;
+    }
+
+    public static int maxProfit(int[] prices) {
+        if (prices.length < 2)
+            return 0;
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+            minPrice = Math.min(minPrice, prices[i]);
+        }
+        return maxProfit;
+    }
+
+    // multiple days
+    public static int maxProfit2(int[] prices) {
+        if (prices.length < 2)
+            return 0;
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (minPrice < prices[i]) {
+                // sell
+                maxProfit += prices[i] - minPrice;
+            }
+            minPrice = prices[i];
+        }
+
+        return maxProfit;
+    }
+
+    public static int maxProfit3(int[] prices) {
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
+        }
+        return maxprofit;
+    }
+}
